58b3335e9705b6da326ead95bcf78506dbdcc9a0
Eduardo <eduardo.er@gmail.com>
2012-11-25 23:01:42 -0500

    Text changes from Gord for home page

--- index.php
+++ index.php
@@ -55,25 +55,27 @@ $isMobile = $detect->isMobile() && !$detect->isTablet();
                     <div class="highlight"></div>
                     <div class="title">News & announcements</div>
                     <div class="description">
-                        Metal Construction Association<br />Premium MCM Fabricator!
+                        Hotel Alma located at the University of Calgary is a recently completed Aluminum Composite Rainscreen Panel system project.
                     </div>
                     <ul class="options">
-                        <li>View Hotel Alma in Portfolio</li>
-                        <li>View <i>*SPECIFIC</i> Panelling</li>
+                        <li><a href="NewsAndAnnouncements.html">View CMC News &amp; Announcments</a></li>
+                        <li><a href="Portfolio.html">View Hotel Alma in Portfolio</a></li>
+                        <li><a href="Surfaces.html">View 4mm Aluminum Composite Rainscreen Panel System, in three custom colours, as Manufactured, Supplied and Installed by Custom Metal.</a></li>
+                        <li><a href="Downloads.html">View CP Series 20 &ndash; See downloads for details</a></li>
                     </ul>
                 </div>
             </div>
             <div class="slide hidden" <?php print($isMobile ? 'data-image-width="1200" data-image-height="798"' : 'data-image-width="1200" data-image-height="798"'); ?> data-slide-duration="8000" data-slide="two">
-                <?php print($isMobile ? '<img src="Images/Small/calgary-tower.jpg" width="1200" height="798" />' : '<img src="Images/Large/calgary-tower.jpg" width="1200" height="798" />'); ?>
+                <?php print($isMobile ? '<img src="Images/Large/calgary-tower.jpg" width="1200" height="798" />' : '<img src="Images/Large/calgary-tower.jpg" width="1200" height="798" />'); ?>
                 <div class="details">
                     <div class="highlight"></div>
                     <div class="title">Calgary Tower</div>
                     <div class="description">
-                        Custom Metal Cladding AP Series Gray and Red Aluminium Panel System
+                        This Iconic local landmark is a proud piece in our vast portfolio.<br/>It is recognizable worldwide and identifies the &quot;Heart of the New West&quot;.
                     </div>
                     <ul class="options">
-                        <li>View in Portfolio</li>
-                        <li>View Aluminium Panel Systems</li>
+                        <li><a href="Portfolio.html">View the Calgary Tower</a></li>
+                        <li><a href="Surfaces.html">View 3.25 mm AP Rainscreen Aluminum panel system as manufactured, Supplied and Installed by Custom Metal.</a></li>
                     </ul>
                 </div>
             </div>
@@ -81,13 +83,13 @@ $isMobile = $detect->isMobile() && !$detect->isTablet();
                 <?php print($isMobile ? '<img src="Images/Large/california-university.jpg" width="1200" height="797" />' : '<img src="Images/Large/california-university.jpg" width="1200" height="797" />'); ?>
                 <div class="details">
                     <div class="highlight"></div>
-                    <div class="title">Metal cladding</div>
+                    <div class="title">Metal roofing and cladding</div>
                     <div class="description">
-                        With its lifetime warranty we are very/proud happy to use Metal Cladding.<br/>We love this shit!
+                        This long lasting maintenance free solution is proven economical.<br/>High quality, light weight roofing system is fabricated on-site to fit the specific dimensions, eliminate seams and help reduce structural framing costs.
                     </div>
                     <ul class="options">
-                        <li>View California University in Portfolio</li>
-                        <li>View <i>*SPECIFIC</i> Panelling</li>
+                        <li><a href="Portfolio.html">View California University in Portfolio</a></li>
+                        <li><a href="Surfaces.html">View CP &ndash; 4mm Composite Aluminum Panel system series 20</a></li>
                     </ul>
                 </div>
             </div>
@@ -97,11 +99,12 @@ $isMobile = $detect->isMobile() && !$detect->isTablet();
                     <div class="highlight"></div>
                     <div class="title">Awards</div>
                     <div class="description">
-                        Current winner of such a prestigious award.<br/>No doubt it came with a title.
+                        CMC has a long history of many prestigious acknowledgements.<br/>Our most recent award is the Alcoa Outstanding Achievement Award.
                     </div>
                     <ul class="options">
-                        <li>View Alberta Childrens Hospital in Portfolio</li>
-                        <li>View <i>*SPECIFIC</i> Panelling</li>
+                        <li><a href="Portfolio.html">View Alberta Childrens Hospital in Portfolio</a></li>
+                        <li><a href="Surfaces.html">View 7/8&quot; corrugated cladding used on the Hospital.</a></li>
+                        <li><a href="NewsAndAnnouncements.html">View Awards in News & Announcements Section</a></li>
                     </ul>
                 </div>
             </div>
@@ -111,11 +114,11 @@ $isMobile = $detect->isMobile() && !$detect->isTablet();
                     <div class="highlight"></div>
                     <div class="title">Telus Science Centre</div>
                     <div class="description">
-                        Custom Metal Cladding AP Series Gray and Red Aluminium Panel System
+                        With its seamless structure this building is an architecture marvel.<br/>It is made with pressure equalized Rainscreen panel System Series 20.
                     </div>
                     <ul class="options">
-                        <li>View in Portfolio</li>
-                        <li>View Aluminium Panel Systems</li>
+                        <li><a href="Portfolio.html">View in Portfolio</a></li>
+                        <li><a href="Surfaces.html">View 4mm Aluminum Composite Material in a pressure equalized Rainscreen panel system. Series 20</a></li>
                     </ul>
                 </div>
             </div>
@@ -125,11 +128,11 @@ $isMobile = $detect->isMobile() && !$detect->isTablet();
                     <div class="highlight"></div>
                     <div class="title">Custom metal contracting</div>
                     <div class="description">
-                        Industry Leadership through Innovation and a commitment to customer satisfaction
+                        Industry Leadership through Innovation and a commitment to customer satisfaction.
                     </div>
                     <ul class="options">
-                        <li>View Alberta Teachers Union in Portfolio</li>
-                        <li>View <i>*SPECIFIC</i> Panelling</li>
+                        <li><a href="Portfolio.html">View Alberta Teachers Union in Portfolio</a></li>
+                        <li><a href="Surfaces.html">View Custom Colour 4mm Composite Aluminum Material in our popular series 20 Rainscreen system.</a></li>
                     </ul>
                 </div>
             </div>
